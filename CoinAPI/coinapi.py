9cd86f0b5d8c0462562d80d5a3293ec3745a18dc
Kyle Hippe <kyle.hippe@gmail.com>
2021-04-28 10:05:27 -0700

    Added symbols

--- CoinAPI/coinapi.py
+++ CoinAPI/coinapi.py
@@ -3,17 +3,19 @@ import sys
 import json
 import requests
 
+established_coins = ["BTC", "ETH", "BNB", "ADA", "DOT", "UNI", "LTC", "LINK", "VET", "XLM"]
+baby_coins = ["AKT", "BAO", "AOA", "RBC", "DAG", "BFC", "MUSH", "FORTH", "ATT", "BNANA"]
 
 def load_keys(keys_path="../private/keys.json"):
     return json.load(open(keys_path))
+
 
 def retrieve_data(keys):
     headers = {"X-CoinAPI-Key": keys['key']}
     base_url = "http://rest.coinapi.io/vi/ohlcv/periods/"
 
 
-
-def retrieve_asset_data(keys, file_save = "../private/asset_data.json"):
+def retrieve_asset_data(keys, file_save="../private/asset_data.json"):
     if os.path.isfile(file_save):
         return json.loads(open(file_save).read())
     else:
@@ -26,6 +28,7 @@ def retrieve_asset_data(keys, file_save = "../private/asset_data.json"):
             json.dump(response.json(), f)
 
         return response.json()
+
 
 def main():
     keys = load_keys()
@@ -42,9 +45,12 @@ def print_coins(coins):
         if i > 10:
             break
 
+
 if __name__ == '__main__':
     if not os.path.isdir("../private"):
-        print("You do not have the keys present, get the keys and place them in the private folder with the filename \"keys.json\"")
+        print(
+            "You do not have the keys present, get the keys and place them in the private folder with the filename "
+            "\"keys.json\"")
         exit = False
         try:
             os.mkdir("../private")
